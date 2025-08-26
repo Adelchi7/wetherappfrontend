f77d3f28f70854a0f35660c728c70ebc654b4993
adelchi <adelchi.pelizzo@gmail.com>
2025-08-26 13:44:02 +0200

    added submit logic

--- script.js
+++ script.js
@@ -46,6 +46,15 @@ async function waitForService() {
 
 waitForService();
 
+async function submitInput(data) {
+  await fetch("/api/submit", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+}
+
+
 
 
 
