d7482c49b08bc68f02d50e5f8b7dbca3d7be6a88
adelchi <adelchi.pelizzo@gmail.com>
2025-09-09 13:55:34 +0200

    added image loader

--- script.js
+++ script.js
@@ -115,30 +115,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-/* const canvas = document.getElementById("bg");
-const ctx = canvas.getContext("2d");
-let w, h, t=0;
-function resize() { w=canvas.width=window.innerWidth; h=canvas.height=window.innerHeight; }
-resize(); window.onresize=resize;
-
-function draw() {
-  t += 0.01;
-  ctx.clearRect(0,0,w,h);
-  for (let i=0;i<20;i++) {
-    ctx.fillStyle=`hsl(${(i*20+t*50)%360},70%,50%)`;
-    ctx.beginPath();
-    ctx.arc(
-      w/2 + Math.sin(t+i)*w/3,
-      h/2 + Math.cos(t+i)*h/3,
-      200,0,Math.PI*2
-    );
-    ctx.fill();
-  }
-  requestAnimationFrame(draw);
-}
-draw(); */
-
-
 
 
 
